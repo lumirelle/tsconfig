f8accc7f3e9bd666190804249d6d92aa07fb8ced
Lumirelle <shabbyacc@outlook.com>
2026-06-07 15:02:10 +0800

    chore: update knip config & list attw and publint as tools

--- knip.config.ts
+++ knip.config.ts
@@ -2,6 +2,5 @@ import type { KnipConfig } from 'knip'
 
 export default {
   entry: ['test/**/*.{js,ts}'],
-  ignoreBinaries: ['mise'],
   ignoreDependencies: ['@lumirelle/oxlint-config'],
 } satisfies KnipConfig
