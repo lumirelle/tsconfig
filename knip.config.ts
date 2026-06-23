4db33cde2060ae10329bac78212b96da10f38fa0
Lumirelle <shabbyacc@outlook.com>
2026-06-23 15:31:46 +0800

    chore: update project setup
    - Move node package setup to package.json, for better consistence
    - Custom ESLint toml rules
    - Add stylua check
    - Add tsc check
    - More clear knip config

--- knip.config.ts
+++ knip.config.ts
@@ -1,7 +1,29 @@
 import type { KnipConfig } from 'knip'
 
 export default {
-  entry: ['test/**/*.{js,ts}'],
-  ignoreFiles: ['taze.config.ts'],
-  ignoreDependencies: ['@lumirelle/oxlint-config', 'taze'],
+  ignoreFiles: [
+    // Test fixtures
+    'test/fixtures/**',
+
+    // TODO(Lumirelle): Create a pull request to add taze plugin for knip.
+    'taze.config.ts',
+  ],
+  ignoreDependencies: [
+    // Check tools
+    '@lumirelle/oxlint-config',
+    '@arethetypeswrong/cli',
+    'publint',
+    'vue-tsc',
+
+    // Test fixtures prerequisites
+    'vue',
+
+    // Dependencies manager
+    'taze',
+
+    // Releasing tools
+    'bumpp',
+    'changelogithub',
+    'pkg-pr-new',
+  ],
 } satisfies KnipConfig
